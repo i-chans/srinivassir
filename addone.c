13079a1cf83cd4da835a09fffaf69ad1811d942d
Chanchal Soni <chanchalsoni5@gmail.com>
2019-04-24 03:35:59 -0700

    Create addone.c

---
+++ addone.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int get_input()
+{
+    int n;
+    printf("enter a number");
+    scanf("%d",&n);
+    return n;
+}
+
+int add_one(int a)
+{
+    a+=1;
+    return a;
+    
+}
+
+void output(int b,int c)
+{
+    printf("value before %d and after adding one %d",b,c);
+}
+
+int main()
+{
+    //int x=get_input();
+    //int y=add_one();
+    add_one(get_input());
+    output(get_input(),add_one(a));
+    
+}
